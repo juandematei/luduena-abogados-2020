2bda6bfa7676b56e018d0f5c980b48650425a4b2
juandematei <juandematei@gmail.com>
2020-07-19 20:33:08 -0300

    desktop version markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
   purge: [],
   theme: {
+    container: {
+      center: true,
+    },
+    listStyleType: {
+      none: 'none',
+      square: 'square',
+    },
     extend: {
       fontFamily: {
         sans: ["Roboto", "sans-serif"]
-      }
+      },
+      colors: {
+        teal: {
+          light: '#5CB1BF',
+          default: '#488B9B',
+          dark: '#3D6D79',
+        },
+        blue: {
+          default: '#2C4F77',
+          dark: '#163859',
+        }
+      },
+
     },
   },
   variants: {},
