8900ddcdbbeec5ce539c83cc3f75f1ba93801e00
juandematei <juandematei@gmail.com>
2020-08-01 11:54:40 -0300

    added favicons

--- dist/publicaciones.js
+++ dist/publicaciones.js
@@ -1,5 +1,212 @@
-let data = JSON.parse('publicaciones.json');
+let data = [
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Un aporte a los conflictos que se aproximan sobre la prescripción de los tributos locales.",
+    "fuente": "Erreius - Suplemento sobre el nuevo código civil unificado (aspectos tributarios, empresariales y contables)",
+    "fecha": "Febrero/Marzo 2015"
+  },
+  {
+    "autor": "Autor del artículo",
+    "titulo": "Empresa Pesquera de la Patagonia y Antártida S.A. c/ Provincia de Tierra del Fuego.",
+    "fuente": "eldial.com - Suplemento sobre Derecho Tributario (Año XVIII - N° 4306)",
+    "fecha": "27/08/15"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "La prescripción de los tributos locales en la jurisprudencia actual del Superior Tribunal de Justicia de la CABA. Críticas a la espera de una resolución de la CSJN.",
+    "fuente": "Errepar - Suplemento Práctica Integral (Año VIII - N° 85.)",
+    "fecha": "Marzo 2016"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Implicancias impositivas de las modificaciones en materia societaria en el nuevo CCyC.",
+    "fuente": "Errepar",
+    "fecha": "Mayo 2016"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "El Aumento Reciente de Tarifas y las Medidas Judiciales.",
+    "fuente": "microjuris.com (mj-doc-9931-ar | mjd9931; mdz,mj)",
+    "fecha": "27/06/16"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Clausura, Monotributo y principio de proporcionalidad de penas a la luz de una jurisprudencia reciente. Comentario al fallo “Echegaray”.",
+    "fuente": "Práctica y Actualidad Tributaria Errepar",
+    "fecha": "Julio 2016"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Algo Más Sobre la Naturaleza Jurídica de los Ilícitos Tributarios.",
+    "fuente": "Doctrina Tributaria Errepar",
+    "fecha": "Agosto 2016"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Régimen de Regularización de Deudas y de Exención de Intereses, Multas y Demás Sanciones.",
+    "fuente": "eldial.com (dc21ae)",
+    "fecha": "30/08/16"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Sinceramiento Fiscal. Comentarios a la Ley 27260 y a su Reglamentación.",
+    "fuente": "Doctrina Tributaria Errepar",
+    "fecha": "Octubre 16"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Blanqueo, Buen Contribuyente y Bloqueo fiscal comentario sobre sus Principales Implicancias.",
+    "fuente": "eldial.com (dc21fa)",
+    "fecha": "05/10/16"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Fallos Disonantes en Materia de Blanqueos.",
+    "fuente": "Doctrina Tributaria Errepar",
+    "fecha": "Enero 17"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "El nuevo impuesto al dólar futuro: ¿tiene futuro?",
+    "fuente": "Diario El Cronista",
+    "fecha": "04/01/17"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Impuesto al dólar futuro y una cuestionable técnica en su creación.",
+    "fuente": "Diario El Cronista",
+    "fecha": "31/01/17"
+  },
+  {
+    "autor": "Autor del artículo",
+    "titulo": "El millonario litigio de Buenos Aires.",
+    "fuente": "Diario El Cronista",
+    "fecha": "27/06/17"
+  },
+  {
+    "autor": "Autor del artículo",
+    "titulo": "El cambio de caratula en el fallo Cristóbal López: ¿Una victoria Pírrica?",
+    "fuente": "Diario El Cronista",
+    "fecha": "27/03/18"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "El impuesto a las ganancias y las indemnizaciones laborales a partir de la reforma de la ley 27430.",
+    "fuente": "eldial.com - Suplemento de Derecho Empresarial",
+    "fecha": "01/12/18"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Fallos Destacados en Materia Tributaria",
+    "fuente": "Revista Electrónica del Departamento de Derecho Económico y Empresarial de la Facultad de Derecho de la UBA (Año 11 - Número 1).",
+    "fecha": "Abril 2019"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "La sanción de clausura y la ley penal más benigna. A la luz de una jurisprudencia reciente de la Cámara de San Martin.",
+    "fuente": "Doctrina Tributaria Errepar - Tomo XL",
+    "fecha": "Diciembre 2019"
+  },
+  {
+    "autor": "Autor del Capítulo VII",
+    "titulo": "Las modificaciones de la reforma tributaria al régimen infraccional tributario.",
+    "fuente": "Procedimiento Tributario y Aduanero después de la reforma (Editorial Marcial Pons)",
+    "fecha": "Año 2018"
+  },
+  {
+    "autor": "Coautor del Capítulo I",
+    "titulo": "Régimen penal tributario después de la reforma.",
+    "fuente": "Régimen penal e infraccional tributario después de la reforma (Editorial Marcial Pons)",
+    "fecha": "Año 2018"
+  },
+  {
+    "autor": "Autor del Capítulo II",
+    "titulo": "Régimen infraccional tributario después de la reforma",
+    "fuente": "Régimen penal e infraccional tributario después de la reforma (Editorial Marcial Pons)",
+    "fecha": "Año 2018"
+  },
+  {
+    "autor": "Autor del Capítulo IX",
+    "titulo": "La posibilidad de aplicar el instituto de la reparación integral a los delitos tributarios.",
+    "fuente": "Derecho Penal Tributario (Editorial Ad Hoc)",
+    "fecha": "Año 2019"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "Comentario sobre la modificación del impuesto a los bienes personales.",
+    "fuente": "Práctica y Actualidad Tributaria Errepar - Tomo XXVI",
+    "fecha": "Enero 2020"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "La nueva moratoria (27541) y los efectos penales.",
+    "fuente": "Doctrina Tributaria Errepar - Tomo XLI",
+    "fecha": "Abril 2020"
+  },
+  {
+    "autor": "Coautor del artículo",
+    "titulo": "TASAS MUNICIPALES: la jurisprudencia pone un límite a la sanción de clausura por incumplimientos materiales -Comentario al fallo “O. S.R.L. c/Municipalidad de San Martín.",
+    "fuente": "eldial.com",
+    "fecha": "22/04/20"
+  }
+];
 
-let articleList = document.querySelector("#article-list");
+function addArticle(e) {
+  //Create HTML elements
+  let article = document.createElement("article");
+  let author = document.createElement("p");
+  let title = document.createElement("h3");
+  let data1 = document.createElement("small");
+  let data2 = document.createElement("small");
+  let icon1 = document.createElement("span");
+  let icon2 = document.createElement("span");
 
-console.log(data);
+  //Define CSS class lists
+  let articleStyle = ["bg-gray-100", "p-4", "rounded", "shadow", "text-gray-700", "text-sm", "flex", "flex-col", "gap-2"];
+  let authorStyle = ["uppercase", "text-sm"];
+  let titleStyle = ["text-xl", "text-blue-dark", "font-bold"];
+  let dataStyle = ["block"];
+  let iconStyle = ["material-icons", "text-base", "align-text-bottom", "mr-1"];
+
+
+
+  //Add CSS class to HTML elements
+  article.classList.add(...articleStyle);
+  author.classList.add(...authorStyle);
+  title.classList.add(...titleStyle);
+  data1.classList.add(...dataStyle);
+  data2.classList.add(...dataStyle);
+  icon1.classList.add(...iconStyle);
+  icon2.classList.add(...iconStyle);
+
+
+  let authorText = document.createTextNode(e.autor)
+  author.appendChild(authorText);
+  article.appendChild(author);
+
+  let titleText = document.createTextNode(e.titulo);
+  title.appendChild(titleText);
+  article.appendChild(title);
+
+  let iconFuente = document.createTextNode("book");
+  icon1.append(iconFuente);
+  data1.append(icon1);
+
+  let dataFuente = e.fuente;
+  data1.append(dataFuente);
+  article.appendChild(data1);
+
+  let iconFecha = document.createTextNode("today");
+  icon2.append(iconFecha);
+  data2.append(icon2);
+
+  let dataFecha = e.fecha;
+  data2.append(dataFecha);
+  article.appendChild(data2);
+
+  //Append articles to list
+  let articleList = document.querySelector("#article-list");
+  articleList.append(article);
+}
+
+data.forEach(e => addArticle(e));
